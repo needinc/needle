34c03a5c31d5b914fbeef97d1d07969c7c62fdda
Chris Lee <chris.r.lee43@gmail.com>
2022-02-28 15:55:51 -0800

    small changes

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # needle
 
+### To Run
 - Clone the repository
 - Run a simple server in the directory `python3 -m http.server` or `python -m SimpleHTTPServer` (python v2)
 - Open `http://localhost:8000/needle.html`
+
+If on Windows:
+- `pip install ipython`
+- `ipython -m http.server 8000`
--- components/game.jsx
+++ components/game.jsx
@@ -31,24 +31,40 @@ const alphabet = [
 const commands = ["enter", "backspace"];
 const allowedKeys = [...alphabet, ...commands];
 
-function getWordMap(word) {
-  const wordMap = {};
-  Array.from(word).forEach((char) => {
-    if (!wordMap[char]) {
-      wordMap[char] = 1;
-    } else {
-      wordMap[char]++;
-    }
-  });
-  return wordMap;
-}
-
 function Game({ word }) {
+  // When true, no further actions can be taken
   const [gameOver, setGameOver] = useState(false);
+
+  // The user's current attempt starting from 0 and ending at 5 for a
+  // total of 6 attempts.
   const [currAttempt, setCurrAttempt] = useState(0);
+
+  // The current key that the user has entered
+  // i.e. "d", "a", "enter", "backspace"
   const [currKey, setCurrKey] = useState("");
+
+  // The letters for each attempt. For example, after 2 attempts, letters may
+  // look like ["other", "snail", "", "", "", ""]
   const [letters, setLetters] = useState(["", "", "", "", "", ""]);
+
+  // Evaluations for each letter of each attempt. An evaluation can be 1 of 3 options:
+  // "correct" => the letter is in the word and in the correct spot
+  // "present" => the letter is in the word but in the wrong spot
+  // "absent" => the letter is not in the word in any spot
+  //
+  // For example if the word is "share",
+  // evaulations for ["other", "snail", "", "", "", ""] would look like
+  // [
+  //        o         t         h          e          r
+  //   ["absent", "absent", "present", "present", "present"],
+  //
+  //        s          n         a          i         l
+  //   ["correct", "absent", "correct", "absent", "absent],
+  //
+  //   [], [], [], []]
   const [evaluations, setEvaluations] = useState([[], [], [], [], [], []]);
+
+  // Show a message to the user
   const [message, setMessage] = useState("");
 
   useEffect(() => {
@@ -94,9 +110,6 @@ function Game({ word }) {
     if (!currKey || gameOver) {
       return;
     }
-
-    // reset message shown to user
-    setMessage("");
 
     // handle key
     if (currKey === "enter") onEnter();
